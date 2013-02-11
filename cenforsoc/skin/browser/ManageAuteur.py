6f673961a9258d98a0839a92114bc1126ee9167d
alain <alain@2b13fa5e-b809-0410-b1a3-83a4c729f18f>
2013-02-11 23:17:46 +0000

    mise en place de la gestion des auteurs. ##

--- cenforsoc/skin/browser/ManageAuteur.py
+++ cenforsoc/skin/browser/ManageAuteur.py
@@ -62,10 +62,10 @@ class ManageAuteur(BrowserView):
         query = session.query(AuteurTable)
         query = query.filter(or_(AuteurTable.auteur_nom.ilike("%%%s%%" % searchString),
                                  or_(AuteurTable.auteur_prenom.ilike("%%%s%%" % searchString))))
-        auteur = ["%s" % (elem.auteur_nom) for elem in query.all()]
+        auteur = ["%s, %s" % ((elem.auteur_nom).upper(), elem.auteur_prenom) for elem in query.all()]
         return auteur
 
-    def getSearchingAuteur(self, auteurPk=None, searchingLetter=None):
+    def getSearchingAuteur(self, auteurPk=None):
         """
         table pg auteur
         recuperation d'un auteur selon la pk
@@ -74,20 +74,21 @@ class ManageAuteur(BrowserView):
         je teste si la pk arrive par param, si pas je prends celle du form
         """
         fields = self.request.form
-        auteurNom = fields.get('auteurNom')
+        auteurNom = fields.get('auteurNom', None)
+
         if not auteurPk:
             auteurPk = fields.get('auteur_pk')
 
         wrapper = getSAWrapper('cenforsoc')
         session = wrapper.session
-        AuteurTable = wrapper.getMapper('Auteur')
+        AuteurTable = wrapper.getMapper('auteur')
         query = session.query(AuteurTable)
         if auteurNom:
+            auteurNom = auteurNom.split(',')
+            auteurNom = auteurNom[0].capitalize()
             query = query.filter(AuteurTable.auteur_nom == auteurNom)
         if auteurPk:
             query = query.filter(AuteurTable.auteur_pk == auteurPk)
-        if searchingLetter:
-            query = query.filter(AuteurTable.auteur_nom.ilike("%%%s" % searchingLetter))
         allAuteurs = query.all()
         return allAuteurs
 
@@ -140,9 +141,15 @@ class ManageAuteur(BrowserView):
             auteur.auteur_prenom = unicode(auteurPrenom, 'utf-8')
 
         session.flush()
-        cible = "%s/creation-d-un-auteur" % (self.context.portal_url(), )
-        self.context.REQUEST.RESPONSE.redirect(cible)
 
+        portalUrl = getToolByName(self.context, 'portal_url')()
+        ploneUtils = getToolByName(self.context, 'plone_utils')
+        message = u"L'auteur %s %s a bien été modifié !" % (auteurNom, auteurPrenom)
+        ploneUtils.addPortalMessage(message, 'info')
+        url = "%s/documentation/auteur/auteur" % (portalUrl)
+        self.request.response.redirect(url)
+        return ''
+        
     def gestionAuteur(self):
         """
         insertion ou update d'un auteur
